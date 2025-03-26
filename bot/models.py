10318d8e3c3d5e8e8ad840fa2939606f28e2e316
Aleksashka301 <aleksashka301@gmail.com>
2025-03-26 22:35:05 +0500

    Added composition model

--- bot/models.py
+++ bot/models.py
@@ -12,3 +12,24 @@ class Bouquet(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.price}₽)"
+
+
+class Composition(models.Model):
+    EVENT_CHOICES = [
+        ('wedding', 'Для свадьбы'),
+        ('march8', '8 марта'),
+        ('teacher', 'Для учителя'),
+        ('no_reason', 'Без повода'),
+    ]
+
+    name = models.CharField('Название композиции:', max_length=100)
+    description = models.TextField('Описание и состав композиции:', blank=True)
+    price = models.IntegerField('Цена в Р:')
+    photo = models.ImageField('Фото композиции:', upload_to='compositions/', blank=True, null=True)
+    is_available = models.BooleanField('В продаже:', default=True)
+    created_at = models.DateTimeField('Дата добавления композиции:', auto_now_add=True)
+    updated_at = models.DateTimeField('Дата последнего изменения:', auto_now=True)
+    event = models.CharField('Мероприятие:', max_length=20, choices=EVENT_CHOICES, default='no_reason')
+
+    def __str__(self):
+        return f"{self.name} ({self.price}₽) — {self.get_event_display()}"
