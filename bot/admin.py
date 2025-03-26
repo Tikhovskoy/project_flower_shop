a0b54256f5a454cfd2d9ac3b20f7188a7edee8af
Aleksashka301 <aleksashka301@gmail.com>
2025-03-26 22:33:51 +0500

    Model connected CompositionAdmin

--- bot/admin.py
+++ bot/admin.py
@@ -2,13 +2,30 @@ from django.contrib import admin
 from django.utils.html import format_html
 
 from FlowerShopBot import settings
-from .models import Bouquet
+from .models import Bouquet, Composition
 
 
 class BouquetAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'is_available', 'created_at', 'updated_at', 'preview')
     list_filter = ('is_available',)
-    list_editable = ('is_available',)
+    list_editable = ('is_available', 'price',)
+
+    def preview(self, obj):
+        if obj.photo:
+            return format_html(
+                '<img src="{}{}" style="width: 50px; height: 50px;"/>',
+                settings.MEDIA_URL, obj.photo
+            )
+        return "Нет фото"
+
+    preview.short_description = "Фото"
+
+
+class CompositionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'event', 'price', 'is_available', 'updated_at', 'preview')
+    list_filter = ('is_available',)
+    list_editable = ('is_available', 'price',)
+    readonly_fields = ('created_at',)
 
     def preview(self, obj):
         if obj.photo:
@@ -22,3 +39,4 @@ class BouquetAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Bouquet, BouquetAdmin)
+admin.site.register(Composition, CompositionAdmin)
