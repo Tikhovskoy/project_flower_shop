474ccb5f5e9f6560a171aae259748f2fd785369d
Aleksashka301 <aleksashka301@gmail.com>
2025-03-27 21:09:40 +0500

    Added order model

--- bot/admin.py
+++ bot/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.utils.html import format_html
-
 from FlowerShopBot import settings
-from .models import Bouquet, Composition
+from .models import Bouquet, Composition, Order
 
 
 class BouquetAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'is_available', 'created_at', 'updated_at', 'preview')
     list_filter = ('is_available',)
     list_editable = ('is_available', 'price',)
+    readonly_fields = ('created_at',)
 
     def preview(self, obj):
         if obj.photo:
@@ -38,5 +38,13 @@ class CompositionAdmin(admin.ModelAdmin):
     preview.short_description = "Фото"
 
 
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer_name', 'order_total', 'status', 'created_at')
+    list_editable = ('status',)
+    list_filter = ('status',)
+    search_fields = ('customer_name', 'phone_number')
+
+
 admin.site.register(Bouquet, BouquetAdmin)
 admin.site.register(Composition, CompositionAdmin)
+admin.site.register(Order, OrderAdmin)
